e60bd34d365adbc663e097a83725290b7601ff05
Kwadwo Agyemang <sam300kye@gmail.com>
2024-01-20 20:46:40 -0500

    Update ProjectsCard.jsx

--- src/Portfolio/ProjectsCard.jsx
+++ src/Portfolio/ProjectsCard.jsx
@@ -2,30 +2,62 @@ import React from "react";
 import collegeBudImg from "../assets/collegeBudImg.png";
 import FwdArrow from "@mui/icons-material/ArrowForward";
 import { Box } from "@mui/material";
+
 const ProjectsCard = (props) => {
   return (
-    <div className="project--cards">
-      <div className="left">
-        <div className="project--image">
-          <div className="image">
-            <img src={collegeBudImg} alt="Project-Img" />
+    <>
+      {ProjectsCardArr.map((cardDetails, cardIndex) => (
+        <div key={cardIndex} className="project--cards">
+          <div className="left">
+            <div className="project--image">
+              <div className="image">
+                <img
+                  src={cardDetails.img}
+                  alt={`ProjectImg-${cardIndex + 1}`}
+                />
+              </div>
+            </div>
+            <Box className="view--details" onClick={props.openTab}>
+              <h4
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                View Details
+                <span className="fwd--arrow">
+                  <FwdArrow />
+                </span>
+              </h4>
+            </Box>
+          </div>
+          <div className="project--details">
+            <p>
+              Title : <span>{cardDetails.title}</span>
+            </p>
+            <p>
+              Languages/Tools:
+              <span>{cardDetails.lang}</span>
+            </p>
           </div>
         </div>
-        <Box className="view--details" onClick={props.openTab}>
-          <h4 style={{ display: "flex", alignItems: "center",justifyContent:"center" }}>
-            View Details
-            <span className="fwd--arrow">
-              <FwdArrow />
-            </span>
-          </h4>
-        </Box>
-      </div>
-      <div className="project--details">
-        <p>Title : <span>College Buddy</span></p>
-        <p>Languages/Tools: <span>JS HTML CSS REACT MUI NODE.JS MONGO-DB EXPRESS.JS </span></p>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
 
 export default ProjectsCard;
+
+const ProjectsCardArr = [
+  {
+    img: collegeBudImg,
+    title: "College Buddy",
+    lang: " JS HTML CSS REACT MUI NODE.JS MONGO-DB EXPRESS.JS",
+  },
+  {
+    img: collegeBudImg,
+    title: "College Buddy",
+    lang: " JS HTML CSS REACT MUI NODE.JS MONGO-DB EXPRESS.JS",
+  },
+];
