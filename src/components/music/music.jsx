01125b635643da4f3a0ab151980c84f8d9079408
Kwadwo Agyemang <sam300kye@gmail.com>
2024-01-24 10:31:03 -0500

    change music

--- src/components/music/music.jsx
+++ src/components/music/music.jsx
@@ -1,14 +1,20 @@
-import React, { useState, useRef } from "react";
-import homeMusicIcon from "../../assets/musiclogo.png";
+import React, { useState, useRef, useEffect } from "react";
 import homeMusicGif from "../../assets/musicGIF.gif";
-import PauseIcon from '@mui/icons-material/Pause';
-import PlayIcon from '@mui/icons-material/PlayArrow';
-import musicGIF from "../../assets/bgSound.mp3";
+import PauseIcon from "@mui/icons-material/Pause";
+import PlayIcon from "@mui/icons-material/PlayArrow";
+// import musicGIF from "../../assets/bgSound.mp3";
+import music from "../../assets/portfolio-music.mP3"
 import "./music.css";
-import { Tooltip, Typography } from "@mui/material";
+import { Tooltip} from "@mui/material";
 const Music = () => {
   const audioRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
+
+  // useEffect(() => {
+  //   audioRef.current.play();
+  //   setIsPlaying(true)
+  // }, []); 
+
 
   const playPauseHandler = () => {
     if (isPlaying) {
@@ -21,18 +27,10 @@ const Music = () => {
 
   return (
     <div>
-      <audio ref={audioRef} src={musicGIF} loop />
+      <audio ref={audioRef} src={music} loop />
       <div className="music--container" onClick={playPauseHandler}>
         <div className="music-paused-icon">
-          <Tooltip
-            title={
-              isPlaying ? (
-                <PauseIcon/>
-              ) : (
-                <PlayIcon/>
-              )
-            }
-          >
+          <Tooltip title={isPlaying ? <PauseIcon /> : <PlayIcon />}>
             <img src={homeMusicGif} alt="homelogo" />
           </Tooltip>
         </div>
